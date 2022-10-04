5264ac87a4a9f4cd6676f941f05b72724cf91d6f
Tdibacco17 <tomasdibacco@gmail.com>
2022-10-04 16:01:08 -0300

    intento nodemailer 2

--- api/src/controllers/mailer/index.js
+++ api/src/controllers/mailer/index.js
@@ -1,38 +1,4 @@
 import nodemailer from "nodemailer";
-import { google } from "googleapis";
-const OAuth2 = google.auth.OAuth2;
-
-const accountTransport = {
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: "tomasdibacco@gmail.com",
-        clientId: "798674056347-15k83ntflno2clqkgq6s4im920c4chdq.apps.googleusercontent.com",
-        clientSecret: "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",
-        refreshToken: "1//04UuHqUP-_C5eCgYIARAAGAQSNwF-L9IrF03IoIGEObV9hZ4BfcC1XENvkIJmmXRqfpaGkVzsiFNKHmkYxqEGndjTNa4GyafGT5g"
-    }
-}
-
-export const mail_rover = async (callback) => {
-    const oauth2Client = new OAuth2(
-        accountTransport.auth.clientId,
-        accountTransport.auth.clientSecret,
-        "https://developers.google.com/oauthplayground",
-    );
-    oauth2Client.setCredentials({
-        refresh_token: accountTransport.auth.refreshToken,
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
-    oauth2Client.getAccessToken((err, token) => {
-        if (err)
-            return console.log(err);;
-        accountTransport.auth.accessToken = token;
-        callback(nodemailer.createTransport(accountTransport));
-    });
-};
-
 
 export const mailer = async (req, res) => {
     const { name, email, phone, message } = req.body;
@@ -43,12 +9,8 @@ export const mailer = async (req, res) => {
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
-            type: 'OAuth2',
             user: "tomasdibacco@gmail.com", // variables de entorno
             pass: "vbllztcqbbpnatbf", // variables de entorno
-            clientId: accountTransport.auth.clientId,
-            clientSecret: accountTransport.auth.clientSecret,
-            refreshToken: accountTransport.auth.accessToken
         },
     });
     // await new Promise((resolve, reject) => {
@@ -79,15 +41,3 @@ export const mailer = async (req, res) => {
         console.log(e.message)
     }
 }
-
-//
-//     try {
-       
-
-//         const result = await transporter.sendMail(info);
-//         console.log(result)
-//         return result;
-//     } catch (e) {
-//         console.log(e.menssage)
-//     }
-// }

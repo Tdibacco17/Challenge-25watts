2719bd3623e6d08bf6906c8320cf98a70363440e
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-10-06 13:35:35 -0300

    armar datos email

--- api/app.js
+++ api/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { router } from "./src/routes/index.js";
 import dotenv from "dotenv"
+// import fs from "fs";
+// import path from "path";
 
 dotenv.config()
 
